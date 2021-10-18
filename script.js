b12fe17dc8d3e45cf457cdc9e6012003ec223128
Farhan <74499472+fb0801@users.noreply.github.com>
2021-10-18 14:55:36 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
 document.addEventListener('DOMContentLoaded', () => {
     const dino = document.querySelector('.dino')
-
-
+    let isJumping = False
+    let gravity = 0.9
 
 function control(e){
     //control the dino
     if(e.keycode === 32) {
-        //
+        if(!isJumping){
+            isJumping = True
+            jump()
+        }
+      
     }
 }
 
@@ -14,24 +18,28 @@ document.addEventListener('keyup', control)
 
 
 function jump(){
-    let position = 0
+    let count = 0
     let timerId = setInterval(function () {
 
             //move dwn
-        if (position ===150){
+        if (position ===15){
             clearInterval(timerId)
             let downTimerId = setInterval(function () {
-                if(position ===0){
+                if(count ===0){
                     clearInterval(downTimerId)
+                    isJumping = false
                 }
                 position-=30
+                count --
                 dino.style.bottom = position +'px'
             },20)
            
         }
 
         //mve up
+        count ++
         position +=30
+        position = position * gravity
         dino.getElementsByClassName.bottom = position + 'px'
     },20)
 }
