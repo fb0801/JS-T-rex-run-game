f54c84f5bf1327c31b5782283ff11f9dc4d9bef3
Farhan <74499472+fb0801@users.noreply.github.com>
2021-10-18 14:26:33 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const dino = document.querySelector('.dino')
+
+
+
+function control(e){
+    //control the dino
+    if(e.keycode === 32) {
+        //
+    }
+}
+
+document.addEventListener('keyup', control)
+
+
+
+
+
+
+})
